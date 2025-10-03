23eceae9af1063564c020b77f0bdb073e2a16cdf
Diego Pertierra <dpertierra@hotmail.com>
2025-10-03 17:44:27 +0200

    Corrige error de compatibilidad con DiegoWTsStarterSelection

--- Modo Random/007_Compatibility.rb
+++ Modo Random/007_Compatibility.rb
@@ -3,9 +3,9 @@ if PluginManager.installed?("DiegoWTs Starter Selection") && defined?(DiegoWTsSt
     alias_method :original_initialize, :initialize unless method_defined?(:original_initialize)
     def initialize(pkmn1, pkmn2, pkmn3)
       original_initialize(pkmn1, pkmn2, pkmn3) unless RandomizedChallenge.enabled?
-      starter1 = get_starter(0) || pkmn1
-      starter2 = get_starter(1) || pkmn2
-      starter3 = get_starter(2) || pkmn3
+      starter1 = get_starter(0)&.species || pkmn1
+      starter2 = get_starter(1)&.species || pkmn2
+      starter3 = get_starter(2)&.species || pkmn3
       RandomizedChallenge.pause
       original_initialize(starter1, starter2, starter3)
       RandomizedChallenge.resume
