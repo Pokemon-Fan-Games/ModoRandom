0282fc44e79bbb210bb361109f0ce61594524938
Diego Pertierra <dpertierra@hotmail.com>
2025-10-03 22:32:28 +0200

    Correcciones DiegoWT

--- Modo Random/007_Compatibility.rb
+++ Modo Random/007_Compatibility.rb
@@ -2,7 +2,7 @@ if PluginManager.installed?("DiegoWTs Starter Selection") && defined?(DiegoWTsSt
   class DiegoWTsStarterSelection
     alias_method :original_initialize, :initialize unless method_defined?(:original_initialize)
     def initialize(pkmn1, pkmn2, pkmn3)
-      original_initialize(pkmn1, pkmn2, pkmn3) unless RandomizedChallenge.enabled?
+      return original_initialize(pkmn1, pkmn2, pkmn3) unless RandomizedChallenge.enabled? 
       starter1 = get_starter(0)&.species || pkmn1
       starter2 = get_starter(1)&.species || pkmn2
       starter3 = get_starter(2)&.species || pkmn3
